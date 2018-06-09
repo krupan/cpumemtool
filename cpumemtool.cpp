f833316d207edff8bf9c4741d6eb003354747f15
Bryan Murdock <bmurdock@gmail.com>
2018-06-09 14:26:54 -0600

    client works

--- cpumemtool.cpp
+++ cpumemtool.cpp
@@ -21,9 +21,9 @@ int main(int argc, char* argv[])
 {
     try
     {
-        if (argc != 3)
+        if (argc != 4)
         {
-            std::cerr << "Usage: blocking_tcp_echo_client <host> <port>\n";
+            std::cerr << "Usage: cpumemtool <host> <port> <command>\n";
             return 1;
         }
 
@@ -37,18 +37,18 @@ int main(int argc, char* argv[])
         boost::asio::connect(s, endpoints);
 
         using namespace std; // For strlen.
-        std::cout << "Enter message: ";
-        char request[max_length];
-        std::cin.getline(request, max_length);
-        size_t request_length = strlen(request);
-        boost::asio::write(s, boost::asio::buffer(request, request_length));
-
+        char command[max_length];
+        // TODO: will argv[3] always be null terminated?
+        strcpy(command, argv[3]);
+        size_t command_length = strlen(command);
+        // add the needed \n:
+        command[command_length] = '\n';
+        command[command_length + 1] = 0;
+        command_length++;
+        boost::asio::write(s, boost::asio::buffer(command, command_length));
         char reply[max_length];
-        size_t reply_length = boost::asio::read(s,
-                                                boost::asio::buffer(reply, request_length));
-        std::cout << "Reply is: ";
+        size_t reply_length = s.read_some(boost::asio::buffer(reply, max_length));
         std::cout.write(reply, reply_length);
-        std::cout << "\n";
     }
     catch (std::exception& e)
     {
