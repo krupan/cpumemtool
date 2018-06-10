35ae629b0fbbc787bb2c13b9d3b7fcbc4d98aa2a
Bryan Murdock <bmurdock@gmail.com>
2018-06-09 19:21:34 -0600

    updated comment headers

--- cpumemd.cpp
+++ cpumemd.cpp
@@ -1,12 +1,5 @@
-//
-// async_tcp_echo_server.cpp
-// ~~~~~~~~~~~~~~~~~~~~~~~~~
-//
-// Copyright (c) 2003-2018 Christopher M. Kohlhoff (chris at kohlhoff dot com)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
+// tcp server for querying cpu load and memory usage
+
 #include <assert.h>
 #include <cstdlib>
 #include <iostream>
--- cpumemtool.cpp
+++ cpumemtool.cpp
@@ -1,12 +1,4 @@
-//
-// blocking_tcp_echo_client.cpp
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//
-// Copyright (c) 2003-2018 Christopher M. Kohlhoff (chris at kohlhoff dot com)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
+// client to querying cpu load and memory usage
 
 #include <cstdlib>
 #include <cstring>
